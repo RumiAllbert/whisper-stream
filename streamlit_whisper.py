caa032851aee9cf2efdac353b3306c97310b3ea3
Rumi Allbert <rumiallbert@gmail.com>
2022-12-24 20:55:00 -0500

    [feat] add caching to model
    Signed-off-by: Rumi Allbert <rumiallbert@gmail.com>

--- streamlit_whisper.py
+++ streamlit_whisper.py
@@ -20,49 +20,68 @@ st.title("Simple Transcriber")
 st.header("Upload an audio file to get started")
 
 # Upload an audio file
-audio_file = st.file_uploader("Upload", type=["mp3", "wav", "m4a"])
+# @st.cache(show_spinner=True, allow_output_mutation=True, suppress_st_warning=True)
+def load_audio_file(audio_file):
+    if audio_file is None:
+        audio_file = st.file_uploader("Upload", type=["mp3", "wav", "m4a"])
+    return audio_file
 
-# Load the model if there is an error loading the model, display an error message to contact the developer
-try:
-    model = whisper.load_model("base")
-except:
-    st.error(
-        "There was an error loading the model. Please contact [email](mailto:youngpractitioners.group@gmail.com) to report this issue."
-    )
 
-# h3 header
-st.markdown("#### Press the button to transcribe the audio file")
+audio_file = load_audio_file(None)
+
+# Load the model
+@st.cache(suppress_st_warning=True)
+def load_model():
+    try:
+        return whisper.load_model("base")
+    except Exception as e:
+        st.error(
+            "There was an error loading the model. Please contact [email](mailto:youngpractitioners.group@gmail.com) to report this issue."
+        )
+
+
+model = load_model()
 
 # Transcribe the audio file
-if st.button("Transcribe", type="primary", help="Transcribe the audio file"):
+@st.cache(suppress_st_warning=True, allow_output_mutation=True, show_spinner=True)
+def transcribe(audio_file, model):
     if audio_file is not None:
+        st.sidebar.empty()
         st.sidebar.success("Transcribing...")
         transcription = model.transcribe(audio_file.name)
         st.sidebar.success("Transcription complete!")
         st.session_state.transcription = transcription["text"]
-    else:
-        st.sidebar.error("Please upload an audio file to get started")
+        return transcription["text"]
+
+
+# If the model and audio file have been loaded, transcribe the audio file
+transcription = None
+if model is not None and audio_file is not None:
+    # h3 header
+    st.markdown("#### Press the button to transcribe the audio file")
+    if model is not None and st.button(
+        "Transcribe", type="primary", help="Transcribe the audio file"
+    ):
+        transcription = transcribe(audio_file, model)
+
+    # Add a line break
+    st.markdown("---")
+
+# Display the original audio file
+if audio_file is not None:
+    st.sidebar.header("Play Original Audio File")
+    st.sidebar.audio(audio_file)
 
 # Display the transcription
-if st.session_state.transcription is not None:
+if transcription is not None:
     st.markdown("## Transcription")
-    st.markdown(st.session_state.transcription)
+    st.write(transcription)
 
-# Clear the transcription
-if st.session_state.transcription is not None:
-    if st.button("Clear transcription"):
-        st.session_state.transcription = None
-
-st.sidebar.header("Play Original Audio File")
-st.sidebar.audio(audio_file)
-
-
-# Download the transcription as a text file if the audio file has been loaded and the transcription has been completed
-if audio_file is not None and st.session_state.transcription is not None:
-    st.sidebar.markdown("#### Download the transcription as a text file")
-    st.sidebar.download_button(
-        label="Download",
-        data=st.session_state.transcription,
+    # Download the transcription as a text file if the audio file has been loaded and the transcription has been completed
+    st.markdown("#### Download the transcription as a text file")
+    st.download_button(
+        label="Download transcription",
+        data=transcription,
         file_name="transcription.txt",
         mime="text/plain",
     )
