61c1390a7282bd60fb410b1a798ba0426461afda
Rumi Allbert <rumiallbert@gmail.com>
2022-12-27 18:43:22 -0500

    Change model

--- streamlit_whisper.py
+++ streamlit_whisper.py
@@ -44,7 +44,7 @@ audio_file = load_audio_file(None)
 @st.cache(suppress_st_warning=True)
 def load_model():
     try:
-        return whisper.load_model("base")
+        return whisper.load_model("medium")
     except Exception as e:
         st.error(
             "There was an error loading the model. Please contact [email](mailto:youngpractitioners.group@gmail.com) to report this issue."
