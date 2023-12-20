10e1f5c5065fe10de0fded6d51ae5c257acbcefd
Rumi Allbert <rumiallbert@gmail.com>
2023-12-20 15:13:01 -0600

    Update use_safetensors flag in AutoModelForSpeechSeq2Seq initialization

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=False
 )
 model.to(device)
 
