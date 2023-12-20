4e86814b7df61d88f530583e172adc14e3bf0789
Rumi Allbert <rumiallbert@gmail.com>
2023-12-20 13:33:09 -0600

    Add speech-to-text functionality using OpenAI's Whisper model

--- src/app.py
+++ src/app.py
@@ -1,141 +1,46 @@
 import tempfile
 import threading
-
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
 import streamlit as st
 import whisper
 import yaml
 from yaml import SafeLoader
+from .constants import languages, MODEL_SIZE
 
+from .config import set_page_config
 from utility import write_srt
 
-MODEL_SIZE = "base.en"
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
 # --------------------------------------------------------------------------------------------------
 # Set the page configuration
 # --------------------------------------------------------------------------------------------------
-st.set_page_config(
-    page_title="Transcriber",
-    page_icon="🤖",
-    layout="wide",
-    menu_items={
-        "Get Help": "https://studentsforfg.org/",
-        "About": """Simple GUI for OpenAI's Whisper.""",
-    },
-)
-
-# List of languages supported by OpenAI's Whisper
-languages = [
-    "Auto Detect",
-    "Afrikaans",
-    "Albanian",
-    "Amharic",
-    "Arabic",
-    "Armenian",
-    "Assamese",
-    "Azerbaijani",
-    "Bashkir",
-    "Basque",
-    "Belarusian",
-    "Bengali",
-    "Bosnian",
-    "Breton",
-    "Bulgarian",
-    "Burmese",
-    "Castilian",
-    "Catalan",
-    "Chinese",
-    "Croatian",
-    "Czech",
-    "Danish",
-    "Dutch",
-    "English",
-    "Estonian",
-    "Faroese",
-    "Finnish",
-    "Flemish",
-    "French",
-    "Galician",
-    "Georgian",
-    "German",
-    "Greek",
-    "Gujarati",
-    "Haitian",
-    "Haitian Creole",
-    "Hausa",
-    "Hawaiian",
-    "Hebrew",
-    "Hindi",
-    "Hungarian",
-    "Icelandic",
-    "Indonesian",
-    "Italian",
-    "Japanese",
-    "Javanese",
-    "Kannada",
-    "Kazakh",
-    "Khmer",
-    "Korean",
-    "Lao",
-    "Latin",
-    "Latvian",
-    "Letzeburgesch",
-    "Lingala",
-    "Lithuanian",
-    "Luxembourgish",
-    "Macedonian",
-    "Malagasy",
-    "Malay",
-    "Malayalam",
-    "Maltese",
-    "Maori",
-    "Marathi",
-    "Moldavian",
-    "Moldovan",
-    "Mongolian",
-    "Myanmar",
-    "Nepali",
-    "Norwegian",
-    "Nynorsk",
-    "Occitan",
-    "Panjabi",
-    "Pashto",
-    "Persian",
-    "Polish",
-    "Portuguese",
-    "Punjabi",
-    "Pushto",
-    "Romanian",
-    "Russian",
-    "Sanskrit",
-    "Serbian",
-    "Shona",
-    "Sindhi",
-    "Sinhala",
-    "Sinhalese",
-    "Slovak",
-    "Slovenian",
-    "Somali",
-    "Spanish",
-    "Sundanese",
-    "Swahili",
-    "Swedish",
-    "Tagalog",
-    "Tajik",
-    "Tamil",
-    "Tatar",
-    "Telugu",
-    "Thai",
-    "Tibetan",
-    "Turkish",
-    "Turkmen",
-    "Ukrainian",
-    "Urdu",
-    "Uzbek",
-    "Valencian",
-    "Vietnamese",
-    "Welsh",
-    "Yiddish",
-    "Yoruba",
-]
+set_page_config()
 
 # --------------------------------------------------------------------------------------------------
 # Constants
@@ -152,6 +57,7 @@ st.title("✍️ Simple Transcriber")
 # Set a h2 header
 st.header("Upload an audio file to get started")
 
+
 # -------------------
 # Functions
 # -------------------
@@ -159,90 +65,76 @@ st.header("Upload an audio file to get started")
 def load_audio_file(audio_file):
     """Load audio file from specified"""
     if audio_file is None:
-        if audio_file := st.file_uploader(
+        audio_file = st.file_uploader(
             label="Audio File", type=["mp3", "wav", "m4a", "wma", "aac"]
-        ):
-            # Store the audio file in the temporary directory
-            with open(f"{temp_dir.name}/{audio_file.name}", "wb") as f:
-                f.write(audio_file.read())
-
-            return f"{temp_dir.name}/{audio_file.name}"
-
-
-# semaphore = threading.Semaphore(1)
-
-
-# @st.experimental_singleton
-@st.cache_resource
-def load_model(size=MODEL_SIZE):
-    """Load whisper model"""
-    # Acquire the Semaphore
-    # semaphore.acquire()
-    try:
-        model = whisper.load_model(size)
-    except Exception as exp:
-        st.error(f"Error loading model.\n{exp}")
-        model = None
-    finally:
-        # Release the Semaphore
-        # semaphore.release()
-        return model
+        )
+        if audio_file:
+            try:
+                # Pass the file object directly to the load_model function if possible
+                return audio_file
+            except Exception as e:
+                st.error(f"Error loading audio file.\n{e}")
+    return None
 
 
 # Load the model and audio file
 audio_file = load_audio_file(None)
 
-# model = load_model()
-if "model" not in st.session_state:
-    st.session_state["model"] = load_model()
-model = st.session_state["model"]
-
 
 @st.cache_data
-def transcribe(audio_file, language):
+def transcribe(audio_file):
     """Transcribe the audio file"""
-    # Acquire the Semaphore
-    # semaphore.acquire()
     try:
         if audio_file is not None:
             with st.spinner("Transcription is currently in progress. Please wait..."):
                 st.sidebar.empty()
                 st.sidebar.success("Transcribing...")
-                transcription = model.transcribe(audio_file, language=language)
-                st.session_state.transcription = transcription["text"]
-                st.session_state.segments = write_srt(transcription["segments"])
-                return transcription["text"]
+
+                # Load the audio file
+                dataset = load_dataset(
+                    "distil-whisper/librispeech_long", "clean", split="validation"
+                )
+                sample = dataset[0]["audio"]
+
+                # Transcribe the audio file
+                result = pipe(sample)
+
+                st.session_state.transcription = result["text"]
+                st.session_state.segments = write_srt(result["segments"])
+                return result["text"]
+    except Exception as e:
+        st.error(f"Error occurred during transcription: {e}")
     finally:
         st.sidebar.success("Transcription complete!")
-        # Release the lock
-        # semaphore.release()
 
 
-# If the model and audio file have been loaded, transcribe the audio file
-transcription = None
 if model is not None and audio_file is not None:
-    # h3 header
+    # Display a header for the transcription button
     st.markdown("#### Press the button to transcribe the audio file")
+
+    # Select the language
     language = st.selectbox("🗣️ Select Language", languages)
-    # If language is not "Auto Detect", or "English", raise a warning message
+
+    # Display a warning if the selected language is not supported
     if language not in ["Auto Detect", "English"]:
         st.warning(
             "The language you have selected is not supported by the model. The model will default to English.\n\nIf you would like to use a different language, please contact the developer."
         )
-    if language in "Auto Detect":
+
+    # Set language to None if "Auto Detect" is selected
+    if language == "Auto Detect":
         language = None
-    if model is not None and st.button(
-        "Transcribe ✨", type="primary", help="Transcribe the audio file"
-    ):
+
+    # Transcribe the audio file when the button is clicked
+    if st.button("Transcribe ✨", type="primary", help="Transcribe the audio file"):
         try:
             # Transcribe the audio file asynchronously
             transcription = transcribe(audio_file, language)
         except Exception as exp:
-            # Display a warning message if the semaphore is already in use
+            # Display an error message if there is an error during transcription
             st.warning(
-                f"There was an error transcribing the audio file. Please try again. If the problem persists, please contact the developer."
+                "There was an error transcribing the audio file. Please try again. If the problem persists, please contact the developer."
             )
-            # print the error
             st.error(exp)
 
 # Add a line break
@@ -282,8 +174,8 @@ temp_dir.cleanup()
 
 # Add a footer to the main area
 st.markdown(
-    '<div style="position: fixed; bottom: 0; width: 100%; text-align: center;">'
-    'Made with ❤️ by <a href="https://studentsforfg.org/">SFFG</a>'
+    '<div style="position: fixed; bottom: 0; width: 100%; text-align: center; background-color: #f5f5f5; padding: 10px;">'
+    '<span style="font-size: 14px;">Made with ❤️ by <a href="https://studentsforfg.org/" style="color: #333;">SFFG</a></span>'
     "</div>",
     unsafe_allow_html=True,
 )
