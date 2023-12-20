bcdf5c7b87ffd9cdfd5f023794b5beb1f1dec461
Rumi Allbert <rumiallbert@gmail.com>
2023-12-20 13:33:16 -0600

    Add set_page_config function to config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,20 @@
 import pathlib
+import streamlit as st
 
 APP_DIR = pathlib.Path(__file__).parent.absolute()
 
 LOCAL_DIR = APP_DIR / "local"
 LOCAL_DIR.mkdir(exist_ok=True)
+
+
+# src/config.py
+def set_page_config():
+    st.set_page_config(
+        page_title="Transcriber",
+        page_icon="🤖",
+        layout="wide",
+        menu_items={
+            "Get Help": "https://studentsforfg.org/",
+            "About": """Simple GUI for OpenAI's Whisper.""",
+        },
+    )
