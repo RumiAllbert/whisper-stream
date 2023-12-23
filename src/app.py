74d7747a66ed75b2627ab6e958278ec646dfb3dc
Rumi Allbert <rumiallbert@gmail.com>
2023-12-22 20:08:11 -0600

    Refactor app.py: Initialize pipeline and set page configuration

--- src/app.py
+++ src/app.py
@@ -7,34 +7,21 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from config import set_page_config
 from constants import languages
 from utility import write_srt
-
-
-# --------------------------------------------------------------------------------------------------
-# Set the page configuration
-# --------------------------------------------------------------------------------------------------
-set_page_config()
+from model import initialize_pipe
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 model_id = "openai/whisper-large-v3"
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=False
-)
-model.to(device)
+# --------------------------------------------------------------------------------------------------
+# Set the page configuration
+# --------------------------------------------------------------------------------------------------
+set_page_config()
 
-processor = AutoProcessor.from_pretrained(model_id)
-
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    max_new_tokens=128,
-    chunk_length_s=30,
-    batch_size=16,
-    return_timestamps=True,
+# Init the pipeline
+pipe = initialize_pipe(
+    model_id=model_id,
     torch_dtype=torch_dtype,
     device=device,
 )
